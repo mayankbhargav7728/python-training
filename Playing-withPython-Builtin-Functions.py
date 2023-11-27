aa8d51d2dff8f5a36bde083ca0707d4a59a9e0ba
Mayank Pulapa <45941267+mayankbhargav7728@users.noreply.github.com>
2023-11-26 19:10:52 -0800

    Add files via upload

---
+++ Playing-withPython-Builtin-Functions.py
@@ -0,0 +1,35 @@
+print("5*6")
+ 
+print('5*6')
+ 
+print(5*6)
+ 
+abs 10.5
+ 
+abs("10.5")
+ 
+abs(10.5)
+ 
+pow 2 5
+ 
+pow(2 5)
+ 
+pow(2, 5)
+ 
+print ( "hellO World" ) 
+ 
+print("Hello"")
+ 
+print("Hello\"") #Escape Sequence
+ 
+print("Hello\nWorld")
+ 
+print("Hello\tWorld")
+ 
+print("Hello\\World")
+ 
+print("Hello\\\\\\World")
+ 
+print('Hello"')
+ 
+print("Hello'World")
