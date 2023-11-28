55f9af4073bef5b54992b790b4a4b30bbd6df756
Mayank Pulapa <45941267+mayankbhargav7728@users.noreply.github.com>
2023-11-27 19:53:31 -0800

    Add files via upload

---
+++ For-loop-with-examples.py
@@ -0,0 +1,16 @@
+for i in range(1, 9):
+   print (i)
+   
+for i in range(1, 11):
+    print(f"5 * {i} = {5 * i}") #formatted string example
+
+
+x = 3 
+y = 3
+ 
+print(f"The sum of {x} and {y} is {x + y}") #formatted string example2
+
+
+
+for i in range (10,0,-1): #step function
+   print(i)
